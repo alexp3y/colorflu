e112cc7a1e7984c5f8b791393e1dcdba71563e94
amperry <alexp3y@gmail.com>
2019-02-25 11:37:06 +0100

    Added new color palette and ship design

--- game.js
+++ game.js
@@ -1,161 +1,245 @@
-const SHIP_RADIUS = 25;
-const BLACK_BUBBLE_RADIUS = 8;
-const COLOR_BUBBLE_RADIUS = 5;
-
+const palette = {
+    red: '#e22b2b',
+    pink: '#f28cb8',
+    darkBlue: '#254b8a',
+    orange: '#fca420',
+    yellow: '#fde02a',
+    lightPurple: '#e3dff7',
+    purple: '#390f59',
+    blue    : '#1627cf',
+    green: '#53a678',
+    maroon: '#ac2a62'
+}
+// Radii
+const SHIP_RADIUS = 35;
+const ENEMY_BUBBLE_RADIUS = 8;
+const AMMO_BUBBLE_RADIUS = 5;
+const BUBBLES_PER_BURST = 50;
+// Velocities
 const SHIP_VELOCITY = 3;
 const BULLET_VELOCITY = 4;
 const SCROLL_ADJUST_VELOCITY = 2;
 
+const GRADIENT_OFFSET = 100;
+// Clock Delays
 const SHIP_TRIGGER_DELAY = 3;
-const BUBBLES_PER_BURST = 50;
-
 const SCROLL_DELAY = 6;
-
-const palette = ['#f0582c',"#c2e57e","#5ce184","#3daae3","#5599de","#d63570","#d63570","#d63570"];
 
 
 class Game {
     constructor(height, width) {
         this.board = new Board(height, width);
-        this.ship = new Ship(this.board.wallStart+SHIP_RADIUS, height/2 ),
+        this.ship = new Ship(this.board.leftScrollWall+SHIP_RADIUS, height/2 ),
         this.paused = false,
-        this.loopCounter = 0,
-        this.score = 0;
+        this.loopCounter = 0;
     }
     updateGame() {
-        this.loopCounter++;
-        if (this.loopCounter > 12) this.loopCounter = 1;  // 12 lets us do delayed effects at modulo 2, 3, 4, 6, 12
-        
+        incrementLoopCounter();
+
         if (!this.paused) {
-            // background scrolling
-            let scrollAdjust = 0;
-            if (this.ship.x+this.ship.r >= this.board.wallEnd 
-                && this.ship.rightGasOn
-                && this.board.gradientEnd >= 0) {
+            // scrolling adjustments
+            let scrollAdjust = 0, scrollBlock = false;
+            if (this.ship.x+this.ship.r >= this.board.rightScrollWall && this.ship.rightGasOn) {
                 if (this.isDelayReady(SCROLL_DELAY)) {
-                    this.board.gradientStart--;
-                    this.board.gradientEnd--;
+                    this.board.increaseGradient();
                 }
                 scrollAdjust = -SCROLL_ADJUST_VELOCITY;
-            }
-            if (this.ship.x-this.ship.r <= this.board.wallStart 
-                && this.ship.leftGasOn 
-                && this.board.gradientStart <= 255) {
+            } else if (this.ship.x-this.ship.r <= this.board.leftScrollWall && this.ship.leftGasOn) {
                 if (this.isDelayReady(SCROLL_DELAY)) {
-                    this.board.gradientStart++;
-                    this.board.gradientEnd++;
+                    this.board.decreaseGradient();
                 }
                 scrollAdjust = SCROLL_ADJUST_VELOCITY;
             } 
 
-            // bubbles
-            this.board.bubbles.forEach((bubble, i) => {
-                if (bubble.color != 'black') {
-                    if (bubble.isCollidedWith(this.ship)) {
-                        this.ship.eatBubble(this.board.bubbles.splice(i,1)[0]);
-                        this.score++;
-                        return;
+            // adjust enemy bursts
+            this.board.enemyBursts.forEach((burst, i) => {
+                burst.bubbles.forEach((enemy, j) => {
+                    if (enemy.isCollidedWith(this.ship)) { // check ship colission
+                        // ship die...
+                        this.board.enemyBursts[i].bubbles.splice(j,1);
+                    } else if (this.board.isElementOutOfBounds(enemy)) { // boundary check
+                        this.board.enemyBursts[i].bubbles.splice(j,1);
+                    } else {
+                        this.board.bullets.forEach((bullet, k) => {  // check bullet colissions
+                            if (enemy.isCollidedWith(bullet)) {
+                                // enemy die...
+                                this.board.enemyBursts[i].bubbles.splice(j,1);
+                                this.board.bullets.splice(k,1);
+                            }
+                        });
                     }
-                } else {
-                    this.board.bullets.forEach((bullet, j) => {
-                        if (bullet.isCollidedWith(bubble)) {
-                            bubble.color = bullet.color;
-                            this.board.bullets.splice(j,1);
-                        }
-                    });
-                }
-                if (bubble.isOutOfBounds()) {
-                    this.board.bubbles.splice(i,1);
-                    this.score--;
-                } else {
-                    bubble.move(scrollAdjust);
-                }
+                });
+                if (burst.bubbles.length == 0) this.board.enemyBursts.splice(i,1);
+                else if (burst.isAtMaxRadius()) burst.reverseBurstDirection();
             });
-            // bullets
-            this.board.bullets.forEach(b => b.move());
+            
+            // adjust ammo bursts
+            this.board.ammoBursts.forEach((burst, i) => {
+                burst.bubbles.forEach((ammo, j) => {
+                    if (ammo.isCollidedWith(this.ship)) {
+                        this.ship.eatBubble(this.board.ammoBursts[i].splice(j,1)[0]);
+                    } else if (this.board.isElementOutOfBounds(ammo)) {
+                        this.board.ammoBursts[i].bubbles.splice(j,1);
+                    }
+                });
+                if (burst.bubbles.length == 0) this.board.ammoBursts.splice(i,1);
+            });
+
+            // adjust ship
+            this.ship.applyGas();
+            this.board.enforceShipBoundary(this.ship);
             if (this.isDelayReady(SHIP_TRIGGER_DELAY)) {
                 this.board.addBullets(this.ship.spitBubbles());
             }
-            // ship
-            this.ship.applyGas();
-            this.ship.enforceBoundary(this.board.wallStart,this.board.wallEnd,0,this.board.h);
-            this.ship.move(0);
+            
+            // move all remaining elements with any detected scroll adjustments
+            moveGameElements(scrollAdjust);
         }
+    }
+    incrementLoopCounter() {
+        this.loopCounter++;
+        if (this.loopCounter > 12) this.loopCounter = 1;  // 12 lets us do delayed effects at modulo 2, 3, 4, 6, 12
     }
     isDelayReady(delay) {
         return this.loopCounter%delay == 0;
+    }
+    moveGameElements(scrollAdjust) {
+        this.board.enemyBursts.forEach(burst => burst.bubbles.forEach(b => b.move(scrollAdjust)));
+        this.board.ammoBursts.forEach(burst => burst.bubbles.forEach(b => b.move(scrollAdjust)));
+        this.board.bullets.forEach(b => b.move(scrollAdjust));
+        this.ship.move(0);
     }
 }
 
 
 class Board {
     constructor (h, w) {
+        this.enemyBursts = [],
+        this.ammoBursts = [],
+        this.bullets = [],
         this.h = h,
         this.w = w,
-        this.bubbles = [],
-        this.bullets = [],
-        this.wallStart = this.w/6,
-        this.wallEnd = this.w*4/6,
-        this.gradientStart = 255,
-        this.gradientEnd = 125;        
+        this.leftBoundary = -this.w/2,
+        this.rightBoundary = this.w*1.5,
+        this.topBoundary = -this.h/2,
+        this.bottomBoundary = this.h*1.5,
+        this.leftScrollActive = false,
+        this.rightScrollActive = true,
+        this.leftScrollWall = this.w/3,
+        this.rightScrollWall = this.w*3/5,
+        this.leftShade = 255,
+        this.rightShade = 255 - GRADIENT_OFFSET,
+        this.foregroundShade = 0;
+    }
+    isElementOutOfBounds(el) {
+        return (el.x < this.leftBoundary) || (el.x > this.rightBoundary) ||
+                (el.y < this.topBoundary) || (el.y > this.bottomBoundary) ? true : false;
+    }
+    enforceShipBoundary(ship) {
+        let leftBound = (this.leftScrollActive) ? this.leftScrollWall : 0;
+        let rightBound = (this.rightScrollActive) ? this.rightScrollWall : this.w;
+        if (ship.x-ship.r <= leftBound) ship.xVel = 0;
+        if (ship.x+ship.r >= rightBound) ship.xVel = 0; 
+        if (ship.y-ship.r <= 0) ship.yVel = 0;
+        if (ship.y+ship.r >= this.h) ship.yVel = 0;
     }
     addBubbleBurst(x, y) {
         if (posNeg() > 0){
             // black and white
             for (let i=0; i<BUBBLES_PER_BURST; i++) {
-                this.bubbles.push(new Bubble(x, y, BLACK_BUBBLE_RADIUS, 'black'));
+                this.bubbles.push(new Bubble(x, y, ENEMY_BUBBLE_RADIUS, 'black'));
             }
         } else {
             // color
             for (let i=0; i<BUBBLES_PER_BURST; i++) {
-                this.bubbles.push(new Bubble(x, y, COLOR_BUBBLE_RADIUS/1., randomColor()));
+                this.bubbles.push(new Bubble(x, y, AMMO_BUBBLE_RADIUS/1., randomColor()));
             }
         }
     }
     addBullets(bullets) {
         bullets.forEach(b => this.bullets.push(b));
     }
+    increaseGradient() {
+        if (this.leftWallShade - this.rightWallShade > GRADIENT_OFFSET || this.rightWallShade == 0) {
+            if (this.leftWallShade > 0) this.leftWallShade--;
+        }
+        if (this.rightWallShade > 0) this.rightWallShade--;
+    }
+    decreaseGradient() {
+        if (this.leftWallShade > GRADIENT_OFFSET) {
+            if (this.rightWallShade < 255) this.rightWallShade++;
+        } 
+        if (this.leftWallShade < 255) this.leftWallShade++;
+    }
+}
+
+
+class Burst {
+    constructor(x, y, r, maxRadius) {
+        this.x = x,
+        this.y = y,
+        this.r = r,
+        this.maxRadius = maxRadius,
+        this.bubbles = []
+    }
+    isAtMaxRadius() {
+        for (let i=0; i<this.bubbles.length; i++) {
+            if (distance(this, this.bubbles[i]) > this.maxRadius) return true;
+        }
+        return false;
+    }
+    reverseBurstDirection() {
+        this.bubbles.forEach(b => {
+            b.xVel *= -1;
+            b.yVel *= -1;
+        })
+    }
 }
 
 
 class MovableElement {
-    constructor(x, y, xVel, yVel) {
+    constructor(x, y, r, xVel, yVel, color) {
         this.x = x,
         this.y = y,
+        this.r = r,
         this.xVel = xVel,
-        this.yVel = yVel;
+        this.yVel = yVel, 
+        this.color = color;
     }
     move(scrollAdjust) {
         this.x += this.xVel+scrollAdjust;
         this.y += this.yVel;
     }
+    isCollidedWith(element) {
+        return distance(this, element) <= (this.r + element.r);
+    }    
 }
-    
+   
 
 class Bubble extends MovableElement {
     constructor(x, y, r, color) {
-        super(x, y, randomVelocity(), randomVelocity());
-        this.r = r,
-        this.color = color,
-        this.bullet = false;
+        super(x, y, r, randomVelocity(), randomVelocity(), color);
     }
-    isOutOfBounds(h,w) {
-        return (this.x+this.r < 0 || this.x-this.r > w
-            || this.y+this.r < 0 || this.y-this.r > h) 
-        ? true 
-        : false
+}
+
+
+class AmmoBubble extends Bubble {
+    constructor(x, y) {
+        super(x, y, AMMO_BUBBLE_RADIUS, randomColor());
     }
-    isCollidedWith(ship) {
-        return distance(this, ship) <= (this.r + ship.r);
+}
+
+
+class EnemyBubble extends Bubble {
+    constructor(x, y) {
+        super(x, y, ENEMY_BUBBLE_RADIUS, 'black');
     }
 }
 
 
 class Ship extends MovableElement {
     constructor(x, y) {
-        super(x, y, 0, 0);
-        this.r = SHIP_RADIUS,
-        this.color = 'black',
+        super(x, y, SHIP_RADIUS, 0, 0, 'black');
         this.upTriggerOn = false,
         this.downTriggerOn = false,
         this.leftTriggerOn = false,
@@ -164,6 +248,17 @@ class Ship extends MovableElement {
         this.downGasOn = false,
         this.leftGasOn = false,
         this.rightGasOn = false,
+        this.ammo = {
+            red: 0,
+            pink: 0,
+            darkBlue: 0,
+            orange: 0,
+            yellow: 0,
+            purple: 0,
+            blue: 0,
+            green: 0,
+            maroon: 0
+        }
         this.bubbles = [];
     }
     applyGas() {
@@ -178,43 +273,34 @@ class Ship extends MovableElement {
             this.xVel = (this.leftGasOn) ? -SHIP_VELOCITY : 0;
         }
     }
-    enforceBoundary(xStart,xEnd,yStart,yEnd) {
-        if (this.x-this.r <= xStart && this.xVel < 0) this.xVel = 0; // left wall
-        if (this.x+this.r >= xEnd && this.xVel > 0) this.xVel = 0; // right wall
-        if (this.y-this.r <= yStart && this.yVel < 0) this.yVel = 0; // top wall
-        if (this.y+this.r >= yEnd && this.yVel > 0) this.yVel = 0; // bottom wall
-    }    
     eatBubble(bubble) {
-        bubble.r = COLOR_BUBBLE_RADIUS;
+        bubble.r = AMMO_BUBBLE_RADIUS;
         this.bubbles.push(bubble);
     }
     spitBubbles() {
-        let spitBubbles = [];
-        if (this.bubbles.length > 0 && this.upTriggerOn) {
-            let spitUp = this.bubbles.pop();
-            spitUp.yVel = -BULLET_VELOCITY;
-            spitBubbles.push(spitUp);
+        let yVel = 0, 
+            xVel = 0,
+            spitBubbles = [];
+        if (this.upTriggerOn) {
+            yVel -= BULLET_VELOCITY;
         }
-        if (this.bubbles.length > 0 && this.downTriggerOn) {
-            let spitDown = this.bubbles.pop();
-            spitDown.yVel = BULLET_VELOCITY;
-            spitBubbles.push(spitDown);
+        if (this.downTriggerOn) {
+            yVel += BULLET_VELOCITY;
         }
-        if (this.bubbles.length > 0 && this.leftTriggerOn) {
-            let spitLeft = this.bubbles.pop();
-            spitLeft.xVel = -BULLET_VELOCITY;
-            spitBubbles.push(spitLeft);
+        if (this.leftTriggerOn) {
+            xVel -= BULLET_VELOCITY;
         }
-        if (this.bubbles.length > 0 && this.rightTriggerOn) {
-            let spitRight = this.bubbles.pop();
-            spitRight.xVel = BULLET_VELOCITY;
-            spitBubbles.push(spitRight);
+        if (this.rightTriggerOn) {
+            xVel += BULLET_VELOCITY;
         }
-        spitBubbles.forEach(s => {
-            s.bullet = true;
-            s.x = this.x;
-            s.y = this.y;
-        });
+        if ((xVel != 0 || yVel !=0) && this.bubbles.length > 0) {
+            let spit = this.bubbles.pop();
+            spit.x = this.x;
+            spit.y = this.y;
+            spit.xVel += xVel;
+            spit.yVel += yVel;
+            spitBubbles.push(spit);
+        }
         return spitBubbles;
     }
 }
@@ -222,7 +308,10 @@ class Ship extends MovableElement {
 
 const posNeg = () => Math.random() < 0.5 ? -1 : 1;
 const randomVelocity = () => Math.random() * posNeg() / 2;
-const randomColor = () => palette[Math.floor(Math.random() * palette.length)];
+const randomColor = () => {
+    let keys = Object.keys(palette);
+    return palette[keys[Math.floor(Math.random() * keys.length)]];
+}
 const distance = (p, q) => { 
     var dx   = p.x - q.x;         
     var dy   = p.y - q.y;         
--- graphics.js
+++ graphics.js
@@ -13,21 +13,35 @@ class Graphics {
     }
     renderGame() {
         this.ctx.clearRect(0,0,this.w,this.h);
-        this.drawBoard(this.game.board);
-        this.drawShip(this.game.ship);
+        this.drawBoard(this.game.board, this.game.level);
+        this.drawShip(this.game.ship, this.game.level);
         this.drawScore(this.game.score);
         if (this.game.paused) {
-            this.drawPause();
+            this.drawPause(this.game.level);
             // other pause actions/animations
         }
     }
-    drawBoard(board) {
+    drawBoard(board, level) {
         let gradient = this.ctx.createLinearGradient(0, 0, board.w, 0);
-        gradient.addColorStop(0, `rgb(${board.gradientStart}, ${board.gradientStart}, ${board.gradientStart})`);
-        gradient.addColorStop(1, `rgb(${board.gradientEnd}, ${board.gradientEnd}, ${board.gradientEnd})`);
-        this.ctx.fillStyle = gradient;
-        this.ctx.fillRect(0, 0, board.w, board.h);
-
+        switch (level) {
+            case 1: // white -> black
+                gradient.addColorStop(0, `rgb(${board.leftWallShade}, ${board.leftWallShade}, ${board.leftWallShade})`);
+                gradient.addColorStop(1, `rgb(${board.rightWallShade}, ${board.rightWallShade}, ${board.rightWallShade})`);
+                this.ctx.fillStyle = gradient;
+                this.ctx.fillRect(0, 0, board.w, board.h);
+                break;
+            case 2: // black -> red
+                gradient.addColorStop(0, `rgb(${board.leftWallShade}, ${board.leftWallShade}, ${board.leftWallShade})`);
+                gradient.addColorStop(1, `rgb(${board.rightWallShade}, ${board.rightWallShade}, ${board.rightWallShade})`);
+                this.ctx.fillStyle = gradient;
+                this.ctx.fillRect(0, 0, board.w, board.h);
+                break;
+            case 3:
+                break;
+        
+            default:
+                break;
+        }
         board.bubbles.forEach(b => {
             this.ctx.beginPath();
             this.ctx.arc(b.x, b.y, b.r, 0, 2 * Math.PI);
@@ -47,23 +61,54 @@ class Graphics {
         })
     }
     drawShip(ship) {
-        this.ctx.fillStyle = ship.color;
-        this.ctx.beginPath();
-        this.ctx.arc(ship.x, ship.y, ship.r, 0, 2 * Math.PI);
-        this.ctx.fill();
+        this.paint();
+        // this.ctx.fillStyle = ship.color;
+        // this.ctx.beginPath();
+        // this.ctx.arc(ship.x, ship.y, ship.r, 0, 2 * Math.PI);
+        // this.ctx.fill();
         
-        let r = ship.r;
-        ship.bubbles.forEach(b => {
-            if (r > 0) {
-                this.ctx.beginPath();
-                this.ctx.arc(ship.x, ship.y, r, 0, 2 * Math.PI);
-                this.ctx.strokeStyle = b.color;
-                this.ctx.linewidth = 5;
-                this.ctx.stroke(); 
-                r++;
-            }
-        })
+        // let r = ship.r;
+        // ship.bubbles.forEach(b => {
+        //     if (r > 0) {
+        //         this.ctx.beginPath();
+        //         this.ctx.arc(ship.x, ship.y, r, 0, 2 * Math.PI);
+        //         this.ctx.strokeStyle = b.color;
+        //         this.ctx.linewidth = 5;
+        //         this.ctx.stroke(); 
+        //         r++;
+        //     }
+        // })
     }
+    paint() { 
+        this.drawHalo(palette.blue, 43, 1);
+        this.drawHalo(palette.green, 63, 1);
+        this.drawHalo(palette.maroon, 103, 1);
+        
+        this.drawCircle('black', 35);
+
+        // this.drawCircle(palette.lightPurple, 35);
+        this.drawCircle(palette.purple, 33);
+        this.drawCircle(palette.lightPurple, 28);
+        this.drawCircle(palette.yellow, 26);
+        this.drawCircle(palette.orange, 23);
+        this.drawCircle(palette.darkBlue, 21);
+        this.drawCircle(palette.pink, 17);
+        this.drawCircle(palette.red, 15);
+    }
+    drawCircle(color, r) {
+        this.ctx.fillStyle = color;
+        this.ctx.beginPath();
+        this.ctx.arc(this.game.ship.x, this.game.ship.y, r, 0, 2 * Math.PI);
+        this.ctx.fill();
+    }
+    
+    drawHalo(color, r, width) {
+        this.ctx.strokeStyle = color;
+        this.ctx.beginPath();
+        this.ctx.lineWidth = width;
+        this.ctx.arc(this.game.ship.x, this.game.ship.y, r, 0, 2 * Math.PI);
+        this.ctx.stroke();
+    }    
     drawScore(score) {
     }
     drawPause(color) {
